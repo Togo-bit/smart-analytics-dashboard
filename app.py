c909d1d97910ebfb41885b53f6bc6a3bdf8d9c4d
Togo-bit <tadisaipratyush@gmail.com>
2026-04-30 10:23:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ def login():
 
     return render_template('login.html')
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
